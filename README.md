c69b568a36862d71b87acd3d99f1efd03520c974
Birger Jesch <birger.jesch@gmail.com>
2021-05-18 10:13:08 +0200

    selection for switch, stop or playing predefined channel implemented

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Record Priorizer Service for TVHeadend
 A service for priorizing recordings on STD (Single Transponder Decryption) environments. This service priorizes recording 
 tasks over normal access (like viewing TV only) on devices, Common Access Modules or other Interfaces, where only single 
 transponder decryption is available. That means this service guarantee recordings when only one encrypted transponder/channel 
-is available at same time by wiping out other clients.
+is available at same time by wiping out or switching the channel of other clients.
 
 This service is only available on client server configurations with TVHeadend as a server available. When running in a multi
 client cluster, all clients must have access to the timers of other devices in this cluster (see TVHeadend configuration, 
--- service.py
+++ service.py
@@ -20,22 +20,72 @@ class Mon(xbmc.Monitor):
         self.settingschanged = True
 
 
+class AddonSettings(object):
+
+    attempts = None
+    margin = None
+    default_action = None
+    default_channel = None
+    priorized_ids = list()
+
+    def get_settings(self):
+        self.attempts = k.getAddonSetting('conn_attempts', sType=NUM, multiplicator=5)
+        self.margin = k.getAddonSetting('margin', sType=NUM)
+        self.default_action = k.getAddonSetting('default_action', sType=NUM)
+        self.default_channel = k.getAddonSetting('channel_number', sType=NUM)
+
+    def read_priorized(self):
+        cgs_ids_priorized = list()
+
+        # resolve priorized channelgroups by name to channelgroup IDs
+
+        cgs_names_priorized = k.getAddonSetting('channelgroups').split(', ')
+        if cgs_names_priorized:
+            query = {'method': 'PVR.GetChannelgroups',
+                     'params': {'channeltype': 'tv'}}
+            response = k.jsonrpc(query)
+
+            # collect priorized channelgroup IDs
+
+            if response:
+                for cg_name_priorized in cgs_names_priorized:
+                    for group in response['channelgroups']:
+                        if group['label'] == cg_name_priorized: cgs_ids_priorized.append(group['channelgroupid'])
+
+            # collect priorized channel IDs
+
+            c_ids = list()
+            for cg_id_priorized in cgs_ids_priorized:
+                query = {'method': 'PVR.GetChannelGroupDetails',
+                         'params': {'channelgroupid': cg_id_priorized}}
+                response = k.jsonrpc(query)
+                if response:
+                    channels = response['channelgroupdetails']['channels']
+                    c_ids.extend([channel['channelid'] for channel in channels])
+
+            k.writeLog('Update priorized channel IDs: {}'.format(c_ids))
+            self.priorized_ids = c_ids
+
+
+# Instantiate objects from classes
+
+monitor = Mon()
+settings = AddonSettings()
+
 # check PVR server availability
 
 _has_pvr = False
 _st = time.time()
 
-attempts = k.getAddonSetting('conn_attempts', sType=NUM, multiplicator=5)
-margin = k.getAddonSetting('margin', sType=NUM)
 
-while not _has_pvr and attempts > 0:
+while not _has_pvr and settings.attempts > 0:
     query = {'method': 'PVR.GetProperties',
              'params': {'properties': ['available']}}
     response = k.jsonrpc(query)
     _has_pvr = True if (response and response.get('available', False)) else False
     if _has_pvr: break
     xbmc.sleep(1000)
-    attempts -= 1
+    settings.attempts -= 1
 
 k.writeLog('Waiting {:.2f} seconds for PVR responses'.format(time.time() - _st))
 
@@ -63,64 +113,68 @@ else:
     sys.exit()
 
 
-def read_priorized():
-    cgs_ids_priorized = list()
+def stop_player(player_id, title):
 
-    # resolve priorized channelgroups by name to channelgroup IDs
+    # Stop player and notify user
+    query = {
+        "method": "Player.Stop",
+        "params": {"playerid": player_id},
+    }
+    response = k.jsonrpc(query)
 
-    cgs_names_priorized = k.getAddonSetting('channelgroups').split(', ')
-    if cgs_names_priorized:
-        query = {'method': 'PVR.GetChannelgroups',
-                 'params': {'channeltype': 'tv'}}
-        response = k.jsonrpc(query)
-        if response:
-            for cg_name_priorized in cgs_names_priorized:
-                for group in response['channelgroups']:
-                    if group['label'] == cg_name_priorized: cgs_ids_priorized.append(group['channelgroupid'])
-
-        # collect priorized channel Ids
-
-        c_ids = list()
-        for cg_id_priorized in cgs_ids_priorized:
-            query = {'method': 'PVR.GetChannelGroupDetails',
-                     'params': {'channelgroupid': cg_id_priorized}}
-            response = k.jsonrpc(query)
-            if response:
-                channels = response['channelgroupdetails']['channels']
-                c_ids.extend([channel['channelid'] for channel in channels])
-                k.writeLog('Update priorized channel IDs: {}'.format(c_ids))
-                return c_ids
-    return False
+    if response == "OK":
+        k.writeLog('Player stopped')
+        k.notify('%s - %s' % (ADDON_NAME, LS(30050)), '%s - %s' % (LS(30051), title))
 
 
-def service(CP=None):
+def switch_channel(channel_id, title):
 
+    # switch to priorized/predefined channel
+    k.writeLog('Switch to channel ID %s' % channel_id)
+    k.notify('%s - %s' % (ADDON_NAME, LS(30052)), '%s \'%s\'' % (LS(30024), title))
+
+    query = {
+        "method": "Player.Open",
+        "params": {"item": {"channelid": channel_id}}
+    }
+    response = k.jsonrpc(query)
+    if response == 'OK':
+        pass
+    else:
+        k.writeLog('Could not switch to channel ID #%s' % channel_id)
+
+
+def service():
+
+    settings.read_priorized()
     while not monitor.abortRequested():
 
         if monitor.waitForAbort(5):
             break
 
         if monitor.settingschanged:
-            CP = read_priorized()
+            settings.get_settings()
+            settings.read_priorized()
             monitor.settingschanged = False
 
-        if not CP: continue
+        if not settings.priorized_ids: continue
 
         # check for priorized timers
 
         isREC = False
         timer_id = None
         timer_title = None
+
         query = {'method': 'PVR.GetTimers',
                  'params': {'properties': ['starttime', 'startmargin', 'istimerrule', 'state', 'channelid', 'title']}}
         response = k.jsonrpc(query)
         if response and response.get('timers', False):
             for timer in response.get('timers'):
-                if timer['channelid'] not in CP or timer['istimerrule'] or timer['state'] == 'disabled':
-                    continue
+                if timer['channelid'] not in settings.priorized_ids or \
+                        timer['istimerrule'] or timer['state'] == 'disabled': continue
                 elif timer['state'] == 'recording' or \
                         time.mktime(time.strptime(timer['starttime'], JSON_TIME_FORMAT)) - \
-                        margin - (timer['startmargin'] * 60) + TIME_OFFSET < int(time.time()):
+                        settings.margin - (timer['startmargin'] * 60) + TIME_OFFSET < int(time.time()):
                     isREC = True
                     timer_id = timer['channelid']
                     timer_title = timer['title']
@@ -132,42 +186,56 @@ def service(CP=None):
 
         # check for player activities and stop player if necessary, collect player properties
 
-        if isREC:
-            props = dict({'title': timer_title})
+        if  not isREC: continue
+
+        props = dict({'title': timer_title})
+        query = {
+            "method": "Player.GetActivePlayers",
+            }
+        response = k.jsonrpc(query)
+
+        if response:
+            props.update({'playerid': response[0].get('playerid', None)})
             query = {
-                "method": "Player.GetActivePlayers",
-                }
+                "method": "Player.GetItem",
+                "params": {"properties": ["title", "season", "episode", "file"],
+                           "playerid": props['playerid']},
+                "id": "VideoGetItem"
+            }
             response = k.jsonrpc(query)
 
             if response:
-                props.update({'playerid': response[0].get('playerid', None)})
+                props.update({'media': response['item'].get('type', None),
+                              'channelid': response['item'].get('id', None)})
+
+            # Player plays anything except tv, continue main loop
+            if props['media'] != 'channel': continue
+
+            # Player plays a channel outside or the same channel as the prorized channel, continue main loop
+            if props['channelid'] not in settings.priorized_ids or props['channelid'] == timer_id: continue
+
+            # use default action from settings
+            k.writeLog('Switch to default action #%s' % settings.default_action)
+
+            if settings.default_action == 0:
+                stop_player(props['playerid'], props['title'])
+            elif settings.default_action == 1:
+                switch_channel(timer_id, props['title'])
+            elif settings.default_action == 2:
                 query = {
-                    "method": "Player.GetItem",
-                    "params": {"properties": ["title", "season", "episode", "file"],
-                               "playerid": props['playerid']},
-                    "id": "VideoGetItem"
+                    "method": "PVR.GetChannels",
+                    "params": {"channelgroupid": "alltv", "properties": ["channelnumber"]}
                 }
                 response = k.jsonrpc(query)
-
                 if response:
-                    props.update({'media': response['item'].get('type', None),
-                                  'channelid': response['item'].get('id', None)})
-
-                if props['media'] != 'channel': continue
-                if props['channelid'] not in CP or props['channelid'] == timer_id: continue
-
-                # Stop player and notify user
-
-                query = {
-                    "method": "Player.Stop",
-                    "params": {"playerid": props['playerid']},
-                }
-                response = k.jsonrpc(query)
-
-                if response == "OK":
-                    k.writeLog('Player stopped')
-                    k.notify('%s - %s' % (ADDON_NAME, LS(30050)),
-                             '%s - %s' % (LS(30051), props['title']))
+                    sw = False
+                    for _channel in response['channels']:
+                        if _channel['channelnumber'] == settings.default_channel and \
+                                _channel['channelid'] not in settings.priorized_ids:
+                            switch_channel(_channel['channelid'], _channel['label'])
+                            sw = True
+                            break
+                    if not sw: stop_player(props['playerid'], props['title'])
     return
 
 
@@ -191,9 +259,4 @@ if __name__ == '__main__':
                     ADDON.setSetting('channelgroups', ', '.join([_clist[i].getLabel() for i in _idx]))
 
     except IndexError:
-        CP = read_priorized()
-        monitor = Mon()
-        service(CP)
-
-
-
+        service()
